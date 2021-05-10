0b2183b49378eb077caa8013b0be683cdbe51b3a
joe <orbitalotter1@gmail.com>
2021-05-11 10:38:11 +1200

    Added class system and added existing cards

--- Joe_Catalog_V1.py
+++ Joe_Catalog_V1.py
@@ -1,1 +1,22 @@
 import easygui as ui
+
+
+class card:
+    def __init__(self, name, strength, speed, stealth, cunning):
+        self.name = name
+        self.strength = strength
+        self.speed = speed
+        self.stealth = stealth
+        self.cunning = cunning
+
+
+card(name="stoneling", strength=7, speed=1, stealth=25, cunning=15)
+card(name="vexscream", strength=1, speed=6, stealth=21, cunning=19)
+card(name="dawnmirage", strength=5, speed=15, stealth=18, cunning=22)
+card(name="blazegolem", strength=15, speed=20, stealth=23, cunning=6)
+card(name="websnake", strength=7, speed=15, stealth=10, cunning=5)
+card(name="moldvine", strength=21, speed=18, stealth=14, cunning=5)
+card(name="vortexwing", strength=19, speed=13, stealth=19, cunning=2)
+card(name="rotthing", strength=16, speed=7, stealth=4, cunning=12)
+card(name="froststep", strength=14, speed=14, stealth=17, cunning=4)
+card(name="wispghoul", strength=17, speed=19, stealth=3, cunning=2)
