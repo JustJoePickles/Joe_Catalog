717129f5c918c4d0e317f2391122dc8a74247d57
joe <orbitalotter1@gmail.com>
2021-05-24 17:52:34 +1200

    Quality of life/error checking for adding a card: Added error checking for an empty name box and gave the user the option to confirm, edit, or delete the card before it's put into the system.

--- Joe_Catalog_V1.py
+++ Joe_Catalog_V1.py
@@ -34,32 +34,55 @@ def error_check(type):
 stats = ["Strength", "Speed", "Stealth", "Cunning"]
 while 1:
     choice = error_check(ui.buttonbox("What do you want to do?", title="Choice",
-                                      choices=["Add new card", "Search for a card", "Delete a card", "View the full deck"]))
+                                      choices=["Add new card", "Search for a card", "Delete a card",
+                                               "View the full deck"]))
     if choice == "Add new card":
-        name = error_check(ui.enterbox("What is the name of your monster?", title="Name"))
-        card_stats = error_check(
-            ui.multenterbox("Enter the stats of your monster (Max is 25 for each stat)", "Stats", stats))
         while 1:
-            all_clear = "yes"
-            for i in range(0, len(card_stats)):
-                try:
-                    int(card_stats[i])
-                    if 1 > int(card_stats[i]) or int(card_stats[i]) > 25:
-                        print(0 / 0)
-
-                except:
-                    error_check(ui.msgbox("A " + stats[i] + " of " + str(card_stats[i]) + " is not valid input\n"
-                                                                                          "Try an integer, or make sure "
-                                                                                          "it is between 1 and 25",
-                                          title="Invalid input"))
-                    replacement_stat = error_check(
-                        ui.integerbox("Please enter a new value for " + stats[i], title="Replacement Value",
-                                      upperbound=25, lowerbound=1))
-                    card_stats[i] = replacement_stat
-                    all_clear = "no"
-            if all_clear == "yes":
-                card(name=name, strength=card_stats[0], speed=card_stats[1], stealth=card_stats[2], cunning=card_stats[3])
+            name = error_check(ui.enterbox("What is the name of your monster?", title="Name"))
+            if name == "":
+                ui.msgbox("Please enter a name")
+            else:
                 break
+
+        while 1:
+            card_stats = error_check(
+                ui.multenterbox("Enter the stats of your monster (Max is 25 for each stat)", "Stats", stats))
+            all_clear = "no"
+            add_card = "yes"
+            while all_clear == "no":
+                all_clear = "yes"
+                for i in range(0, len(card_stats)):
+                    try:
+                        int(card_stats[i])
+                        if 1 > int(card_stats[i]) or int(card_stats[i]) > 25:
+                            print(0 / 0)
+
+                    except:
+                        error_check(ui.msgbox("A " + stats[i] + " of " + str(card_stats[i]) + "is not valid "
+                                                                                              "input\nTry an integer,"
+                                                                                              " or make sure it is "
+                                                                                              "between 1 and 25",
+                                              title="Invalid input"))
+                        replacement_stat = error_check(
+                            ui.integerbox("Please enter a new value for " + stats[i], title="Replacement Value",
+                                          upperbound=25, lowerbound=1))
+                        card_stats[i] = replacement_stat
+                        all_clear = "no"
+            if all_clear == "yes":
+                add_to_deck = error_check(ui.buttonbox(
+                    "".join(["Here is your card:\n", name.title(), ":\nStrength: ", str(card_stats[0]), "\nSpeed: ",
+                             str(card_stats[1]),
+                             "\nStealth: ", str(card_stats[2]), "\nCunning: ", str(card_stats[3]),
+                             "\n\nWould you like to add it to the deck or edit the values"]),
+                    choices=["Add to deck", "Edit", "Discard"]))
+                if add_to_deck == "Add to deck":
+                    break
+                if add_to_deck == "Discard":
+                    add_card = "no"
+                    break
+
+        if add_card == "yes":
+            card(name=name, strength=card_stats[0], speed=card_stats[1], stealth=card_stats[2], cunning=card_stats[3])
 
     if choice == "Search for a card":
         None
@@ -70,5 +93,5 @@ while 1:
         for monster in card.instances:
             full_card_deck.extend(
                 [monster.name.title(), ":\n\nStrength: ", str(monster.strength), "\nSpeed: ", str(monster.speed),
-                 "\nStealth: ", str(monster.stealth), "\nCunning: ", str(monster.cunning),"\n\n\n"])
-        ui.codebox(msg="The entire deck", text="".join(full_card_deck))
+                 "\nStealth: ", str(monster.stealth), "\nCunning: ", str(monster.cunning), "\n\n\n"])
+        error_check(ui.codebox(msg="The entire deck", text="".join(full_card_deck)))
